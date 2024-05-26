d68d7068aa1a068f31c4ed19338d8235c6f0258d
GabrielaSrb <97604510+GabrielaSrb@users.noreply.github.com>
2024-05-26 15:45:31 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -10,6 +12,8 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Authenticator>
       <App />
+    </Authenticator>
   </React.StrictMode>,
 )
